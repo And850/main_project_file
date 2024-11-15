6adbc1ece2ae090d7c7e144eaee667f47acb41c6
And850 <aryzhanskiy09@gmail.com>
2024-11-14 20:57:13 -0800

    base code starter

--- main_project_file/main.cpp
+++ main_project_file/main.cpp
@@ -1,14 +1,58 @@
-//
-//  main.cpp
-//  main_project_file
-//
-//  Created by Andrey Ryzhanskiy on 14.11.2024.
-//
-
+#pragma GCC optimize("O3")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt")
+#include <bitset>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <fstream>
+#include <cstdlib>
+#include <stdio.h>
+#include <stack>
+#include <random>
+#include <string>
+#include <functional>
+#include <chrono>
+#include <numeric>
+#include <climits>
+#include <tuple>
+#include <cstring>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+typedef string str;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ld, ld> pld;
+typedef pair <ll, pair <ll, ll> > tll;
+const ll dim = 200003;
+const ll inf = LONG_LONG_MAX-1;
+const int iinf = INT_MAX-1;
+const ll mod = 1000000007;
+#define S second
+#define F first
+#define endl '\n'
+//ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    
+    
     return 0;
 }
+
