f41248ba4bf9d38e740bdad196264b649c2310a3
And850 <aryzhanskiy09@gmail.com>
2024-11-14 20:53:27 -0800

    Initial Commit

---
+++ main_project_file/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  main_project_file
+//
+//  Created by Andrey Ryzhanskiy on 14.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
